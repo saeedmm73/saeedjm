5cb08da2fb6724012374d2de54beffe8534b422b
saeed jm <saeedjmdeath@gmail.com>
2022-12-31 21:29:07 +0330

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="colo">
+    <h2 id="bold">session one</h2><br/>
+    <p>Hello {{ name }} {{ lastname }}, we are learning vue!</p>
+    <input :value="name" @input="clickon()" ref="zz">
+    <input :value="lastname" @input="clickon()" ref="la">
+  </div>
+</template>
+<style scoped>
+.colo {
+  background-color: blue;
+  padding: 10px;
+  color: aliceblue;
+}
+#bold {
+font-weight: bold;
+}
+</style>
+
+ <script>
+  export default {
+    name:"v1",
+    data() {
+      return {
+        name : "saeed",
+        lastname : "moqadam",
+      }
+    },
+    methods : {
+      clickon() {
+        this.name = this.$refs.zz.value;
+        this.lastname = this.$refs.la.value
+      }
+    }
+
+  }
+ </script>
